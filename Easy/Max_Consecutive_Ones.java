45466f7e0a96e5516b29def5d84015458d75d047
Distortion <1270640138@qq.com>
2022-06-13 22:01:55 -0500

    220613 LC485

--- Easy/Max_Consecutive_Ones.java
+++ Easy/Max_Consecutive_Ones.java
@@ -23,4 +23,24 @@ public class Max_Consecutive_Ones {
         }
         return maxOnes;
     }
+
+//    Official Result
+//    public int findMaxConsecutiveOnes(int[] nums) {
+//        if (nums.length == 0 || nums == null)
+//            return 0;
+//        int maxCount = 0, count = 0;
+//        int n = nums.length;
+//        for (int i = 0; i < n; i++) {
+//            if (nums[i] == 1) {
+//                count++;
+//            } else {
+//                maxCount = Math.max(maxCount, count);
+//                count = 0;
+//            }
+//        }
+//        maxCount = Math.max(maxCount, count);
+//        return maxCount;
+//    }
+
+
 }
