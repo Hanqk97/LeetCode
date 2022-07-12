f9785c94fc451a597a197c4efd1cbb3c6aa8a6e6
Hnaqk97 <1270640138@qq.com>
2022-07-11 19:26:44 -0500

    220711 LC151

---
+++ First Round/Medium/LC151UR_Reverse_Words_in_a_String.java
@@ -0,0 +1,112 @@
+package Medium;
+/*
+Given an input string s, reverse the order of the words.
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+Return a string of the words in reverse order concatenated by a single space.
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+Example 1:
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+Constraints:
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ */
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class LC151UR_Reverse_Words_in_a_String {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split(" +");
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
+    }
+    //时间复杂度：O(n)，其中 nn 为输入字符串的长度。
+    //空间复杂度：O(n)，用来存储字符串分割之后的结果。
+
+    /*
+    Official Solution2
+    public String reverseWords(String s) {
+        StringBuilder sb = trimSpaces(s);
+
+        // 翻转字符串
+        reverse(sb, 0, sb.length() - 1);
+
+        // 翻转每个单词
+        reverseEachWord(sb);
+
+        return sb.toString();
+    }
+
+    public StringBuilder trimSpaces(String s) {
+        int left = 0, right = s.length() - 1;
+        // 去掉字符串开头的空白字符
+        while (left <= right && s.charAt(left) == ' ') {
+            ++left;
+        }
+
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') {
+            --right;
+        }
+
+        // 将字符串间多余的空白字符去除
+        StringBuilder sb = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+
+            if (c != ' ') {
+                sb.append(c);
+            } else if (sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);
+            }
+
+            ++left;
+        }
+        return sb;
+    }
+
+    public void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char tmp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, tmp);
+        }
+    }
+
+    public void reverseEachWord(StringBuilder sb) {
+        int n = sb.length();
+        int start = 0, end = 0;
+
+        while (start < n) {
+            // 循环至单词的末尾
+            while (end < n && sb.charAt(end) != ' ') {
+                ++end;
+            }
+            // 翻转单词
+            reverse(sb, start, end - 1);
+            // 更新start，去找下一个单词
+            start = end + 1;
+            ++end;
+        }
+    }
+
+    时间复杂度：O(n)，其中 n 为输入字符串的长度。
+    空间复杂度：Java 和 Python 的方法需要 O(n) 的空间来存储字符串。
+
+     */
+    public static void main(String[] args) {
+        LC151UR_Reverse_Words_in_a_String a = new LC151UR_Reverse_Words_in_a_String();
+        String t1 = "a good   example";
+        System.out.println(a.reverseWords(t1));
+    }
+}
